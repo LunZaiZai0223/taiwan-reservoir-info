5ab8b1af324e077fe41fdd490f3fb59e1fa95cfd
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-21 22:04:52 +0800

    feat: add chart-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ReservoirChartComponent } from './components/reservoir-chart/reservoir-chart.component';
+import { ChartListComponent } from './pages/chart-list/chart-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, ReservoirChartComponent],
+  declarations: [AppComponent, LayoutComponent, ReservoirChartComponent, ChartListComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgApexchartsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/chart-list/chart-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+
+import { ReservoirService } from 'src/app/services/reservoir.service';
+
+import { GetReservoir } from 'src/app/models/GetReservoir.model';
+
+@Component({
+  selector: 'app-chart-list',
+  templateUrl: './chart-list.component.html',
+  styleUrls: ['./chart-list.component.scss'],
+})
+export class ChartListComponent implements OnInit, OnDestroy {
+  areaList: string[] = ['north', 'middle', 'south'];
+  northernReservoirList: GetReservoir[] = [];
+  middleReservoirList: GetReservoir[] = [];
+  southernReservoirList: GetReservoir[] = [];
+
+  subscription: Subscription = new Subscription();
+
+  constructor(private reservoirService: ReservoirService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.reservoirService.onReservoirData().subscribe(data => {
+      this.areaList.forEach(area => {
+        this.setReservoirListByArea(area, data);
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  setReservoirListByArea(area: string, rawData: any): void {
+    const wrapper: { [key: string]: string } = {
+      north: 'northernReservoirList',
+      middle: 'middleReservoirList',
+      south: 'southernReservoirList',
+    };
+    const foundKey: any = wrapper[area];
+    this[foundKey as keyof ChartListComponent] = rawData.filter((item: any) => item.area === area);
+  }
+}
