78892035956c25eb6a9775f8ae41b0a9ec83c350
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-21 18:05:10 +0800

    core: add constant list by id - 有先查詢水庫位置，再依照 id 比對地區

---
+++ src/app/constant/reservoir/reservoir-area-by-id.constant.ts
@@ -0,0 +1,14 @@
+export const RESERVOIR_AREA_BY_ID = [
+  {
+    area: 'north',
+    idList: [0, 1, 2, 4, 7, 20],
+  },
+  {
+    area: 'middle',
+    idList: [3, 5, 6, 8, 9, 10, 19],
+  },
+  {
+    area: 'south',
+    idList: [12, 13, 14, 15, 16, 17, 18, 21],
+  },
+];
