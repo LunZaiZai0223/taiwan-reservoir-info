e4d6efbdb95133936ad97dafbac50e5d513f3b70
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-21 22:09:00 +0800

    feat: basic desktop layout and features
    - 更新時間未做（可能要用 pipe?）
    - 詳細內容的功能未做
    - 詳細資料的 data 未整理
    - 路由未設定
    - 黑暗 及 一般 mode 未實作

--- src/app/components/reservoir-chart/reservoir-chart.component.ts
+++ src/app/components/reservoir-chart/reservoir-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 import { ChartComponent } from 'ng-apexcharts';
 
@@ -10,21 +10,42 @@ import { ReservoirAreaChartModel } from '../../models/reservoir-area-chart-model
   templateUrl: './reservoir-chart.component.html',
   styleUrls: ['./reservoir-chart.component.scss'],
 })
-export class ReservoirChartComponent implements OnInit {
+export class ReservoirChartComponent implements OnChanges, OnInit {
   @ViewChild('chart', { static: true }) chart!: ChartComponent;
   @Input() chartData: any[] = [];
   @Input() reservoirName: string = '';
   @HostBinding('class') classes = 'chart';
 
+  /**
+   * > 70%: #008FFB
+   * > 40%: #00E396
+   * < 30%: #FF4560
+   */
+  color: string = '';
+
   chartOptions!: Partial<ReservoirAreaChartModel>;
 
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setChartColor(changes['chartData'].currentValue);
+  }
 
   ngOnInit(): void {
     this.setChart();
   }
 
   private setChart(): void {
-    this.chartOptions = { ...reservoirAreaChartConfig, series: [{ data: this.chartData }] };
+    this.chartOptions = { ...reservoirAreaChartConfig, series: [{ data: this.chartData }], colors: [this.color] };
+  }
+
+  private setChartColor(chartData: any[]): void {
+    if (chartData[0] >= 70) {
+      this.color = '#008FFB';
+    } else if (chartData[0] >= 40) {
+      this.color = '#00E396';
+    } else {
+      this.color = '#FF4560';
+    }
   }
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 import { ReservoirService } from '../services/reservoir.service';
+import { SessionStorageService } from '../services/session-storage.service';
+import { RESERVOIR_AREA_BY_ID } from '../../app/constant/reservoir/reservoir-area-by-id.constant';
 
 @Component({
   selector: 'app-layout',
@@ -7,15 +10,50 @@ import { ReservoirService } from '../services/reservoir.service';
   styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
-  constructor(private reservoirService: ReservoirService) {}
+  @HostBinding('class') class = 'bg-dark';
+
+  constructor(private reservoirService: ReservoirService, private sessionStorageService: SessionStorageService) {}
 
   ngOnInit(): void {
-    // this.processFetchReservoirProcess();
+    const reservoirDataInSessionStorage: string | null = this.checkReservoirDataIsInSessionStorage();
+    if (reservoirDataInSessionStorage) {
+      this.reservoirService.setReservoirList(JSON.parse(reservoirDataInSessionStorage));
+      this.reservoirService.sendReservoirData();
+    } else {
+      this.processFetchReservoirProcess();
+    }
   }
 
   private processFetchReservoirProcess(): void {
-    this.reservoirService.getReservoirDetail().subscribe(result => {
-      console.log(result);
-    });
+    this.reservoirService
+      .getReservoirDetail()
+      .pipe(
+        map(response => {
+          return Object.values(response)
+            .filter(item => typeof item === 'object')
+            .map(item => {
+              const [idNum]: number[] = item.id.match(/\d+/g)!.map(num => +num);
+              let area: string = '';
+              for (const item of RESERVOIR_AREA_BY_ID) {
+                if (item.idList.includes(idNum)) {
+                  area = item.area;
+                  break;
+                }
+              }
+
+              return { ...item, percentage: [+item.percentage, +item.percentage], area };
+            });
+        })
+      )
+      .subscribe(result => {
+        console.log(result);
+        this.reservoirService.setReservoirList(result);
+        this.reservoirService.sendReservoirData();
+        this.sessionStorageService.setSessionStorage({ key: 'reservoir', value: result });
+      });
+  }
+
+  private checkReservoirDataIsInSessionStorage(): string | null {
+    return this.sessionStorageService.getSessionStorage('reservoir');
   }
 }
