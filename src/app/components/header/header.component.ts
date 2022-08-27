011cea51ec60394a55c7f4b16ee96885d9a1b502
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-27 23:37:17 +0800

    feat: change current display mode
    - default is dark

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  currentMode: string = 'dark';
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    this.setBodyModeClass();
   }
 
+  onSwitchMode(mode: string): void {
+    console.log(mode);
+    this.currentMode = mode;
+    this.setBodyModeClass();
+  }
+
+  private setBodyModeClass(): void {
+    switch (this.currentMode) {
+      case 'dark':
+        this.renderer.removeClass(document.body, 'light');
+        this.renderer.addClass(document.body, 'dark');
+        break;
+      case 'light':
+        this.renderer.removeClass(document.body, 'dark');
+        this.renderer.addClass(document.body, 'light');
+        break;
+    }
+  }
 }
