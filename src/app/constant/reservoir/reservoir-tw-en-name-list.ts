ca8b0f3adb1a2fa5fe3c85b269ee912d8de806cd
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-28 23:20:21 +0800

    feat: add i18n files and reservoir name list

---
+++ src/app/constant/reservoir/reservoir-tw-en-name-list.ts
@@ -0,0 +1,24 @@
+/** 水庫中英文名稱表 */
+export const RESERVOIR_TW_EN_NAME_LIST: { [key: string]: string } = {
+  石門水庫: 'Shi-Men Reservoir',
+  寶山第二水庫: 'Baoshan Second Reservoir',
+  翡翠水庫: 'Fei-Cui Reservoir',
+  新山水庫: 'Xinshan Reservoir',
+  寶山水庫: 'Baoshan Reservoir',
+  德基水庫: 'TeChi Reservoir',
+  日月潭水庫: 'Sun Moon Lake Reservoir',
+  湖山水庫: 'Hu-Shan Reservoir',
+  鯉魚潭水庫: 'Liyutan Reservoir',
+  霧社水庫: 'Wushe Reservoir',
+  永和山水庫: 'Yongheshan Reservoir',
+  明德水庫: 'Mingde Reservoir',
+  石岡壩: 'Shigang Dam',
+  南化水庫: 'Nanhua Reservoir',
+  烏山頭水庫: 'Wushantou Reservoir',
+  蘭潭水庫: 'Lantan Lake',
+  曾文水庫: 'Ceng-Wen Reservoir',
+  白河水庫: 'Baihe Reservoir',
+  仁義潭水庫: 'Renyitan Reservoir',
+  阿公店水庫: 'A-Gong-Dian Reservoir',
+  牡丹水庫: 'Mu-Dan Reservoir'
+};
