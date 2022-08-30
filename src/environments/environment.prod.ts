6f46bb3c9189a4983757586134c3058b6f137d82
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-31 00:37:31 +0800

    fix: add production environment properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  // 從別的專案拿到的 API 網址（這邊為主要的資訊來源 API）
+  detailApiUrl:
+    'https://script.googleusercontent.com/macros/echo?user_content_key=LVIDQmhSrWOPABLg1bW6UsiPCQc3LObYzdtfdNIiTwCvd2hgbA8V9Pzt9tmcwrM4xnPJuOiBYWnrCq3CIe6rsbgRNXUHfIt5m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnC02W9NgtydlVxBi8ZVYNE3EaC5kBapbMQehFxYBO5VeiAgulVhFrmJaBxDi1Dzff3V8hGIfOYGuVVugCyiZpB7PpP6BIrbXzw&lib=MUS6h8Wvwtj_6r7KE-7vi86mwDtmjqP8O',
+  // 政府公開的 API 網址
+  additionalApiUrl: 'https://fhyv.wra.gov.tw/FhyWeb/v1/Api/Reservoir/Visual?$format=JSON',
 };
