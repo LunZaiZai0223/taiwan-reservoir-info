78aede1a4dc8add9a1dc588d34727a0755fdd4dc
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-08-31 10:18:11 +0800

    fix: use correct relative path for img tags and static data list

--- src/app/constant/reservoir/reservoir-description-list.ts
+++ src/app/constant/reservoir/reservoir-description-list.ts
@@ -7,14 +7,14 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '寶山第二水庫，簡稱寶二水庫，位於台灣新竹縣寶山鄉山湖村境內之離槽水庫，屬中港溪水系峨眉溪支流石井溪上游，水源引用自頭前溪支流上坪溪（上坪攔河堰）之餘水調節運用，有效蓄水量為3,218萬噸。寶山第二水庫與寶山水庫聯合運用，可增加新竹地區水資源之調蓄及供給能力。',
     descriptionEn:
       'The Baoshan Second Dam (traditional Chinese: 寶山第二水庫; simplified Chinese: 宝山第二水库; pinyin: Bǎoshān Dì Èr Shuǐkù) is a dam in Baoshan Township, Hsinchu County, Taiwan.',
-    locationImgPath: '../../../../assets/location-imgs/寶山第二水庫.png',
+    locationImgPath: 'assets/location-imgs/寶山第二水庫.png',
   },
   {
     id: 'reservoir1',
     descriptionTw:
       '翡翠水庫是台灣臺北都會區的主要水庫之一，位於新店溪支流北勢溪之上，水壩距臺北市區約30公里，庫區範圍涵蓋新北市新店區、石碇區與坪林區，總容量約4億6百萬立方公尺，為臺灣第二大，集水區範圍則涵蓋新北市新店區、石碇區、坪林區與雙溪區，總面積303平方公里。北勢溪、南勢溪和、大漢溪為供應大臺北地區用水主要來源，並附帶發電效益；但與同樣供給臺北都會區公共用水的石門水庫不同的是，該水庫由臺北市政府設置臺北翡翠水庫管理局管理，且不開放觀光，若要進入庫區需經過登記申請許可。翡翠水庫集水區依《都市計畫法》劃定為台北水源特定區，是台灣目前唯一水源特定區，水庫周邊屬限制開發，是全臺水庫集水區管制最嚴格的，有效蓄水率維持在八成以上，並為了永續發展，不輕易允許開發。',
     descriptionEn: 'Fei-Cui reservoir.',
-    locationImgPath: '../../../../assets/location-imgs/翡翠水庫.png',
+    locationImgPath: 'assets/location-imgs/翡翠水庫.png',
   },
   {
     id: 'reservoir7',
@@ -22,14 +22,14 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '德基水庫，原稱為達見水庫，位於台灣台中市和平區大甲溪上游，即台8線中橫公路62公里處，是隸屬於台灣電力公司的一個高山型水庫，為台中地區最大水庫。德基水庫於1969年12月8日開工興建，1973年12月水庫開始蓄水，1973年6月26日達到首期發電初步需求，1974年9月全部完成。完工時，由當時的總統蔣中正命名為「德基水庫」。（語出春秋時代鄭國大臣子產名言：「德，國家之基也。」）',
     descriptionEn:
       'Techi Dam (Chinese: 德基水壩; pinyin: Déjī Shuǐbà; Pe̍h-ōe-jī: Tek-ki Chúi-pà) is a concrete thin arch dam on the Dajia River in Heping District, Taichung, Taiwan. Forming the 454 ha (1,120-acre) Techi Reservoir (德基水庫), the dam is built in the Tachien Gorge in Heping District, providing hydroelectric power, irrigation water, and some flood control, and is operated by the Taiwan Power Company. At 180 m (590 ft), it is the highest dam in Taiwan and one of the tallest dams in the world. The dam was completed in 1974 after five years of construction.',
-    locationImgPath: '../../../../assets/location-imgs/德基水庫.png',
+    locationImgPath: 'assets/location-imgs/德基水庫.png',
   },
   {
     id: 'reservoir2',
     descriptionTw:
       '石門水庫是台灣北部的主要水庫之一，位於桃園市大溪區、龍潭區、復興區與新竹縣關西鎮之間的石門峽谷。石門水庫曾經是遠東最大的水庫，採土石堤岸型壩體，攔截大漢溪溪水蓄水而成，是台灣第一座多功能水庫。',
     descriptionEn: 'Shi-Men reservoir.',
-    locationImgPath: '../../../../assets/location-imgs/石門水庫.png',
+    locationImgPath: 'assets/location-imgs/石門水庫.png',
   },
   {
     id: 'reservoir0',
@@ -37,7 +37,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '主要流入大武崙溪、 主要流出基隆河。新山水庫，位於臺灣基隆市安樂區，即基隆市中心西方3.4公里處、基隆河支流大武崙溪中游新山溪上，距基隆港約3公里，隸屬於臺灣自來水公司第一區管理處所管理的水庫，主要為公共給水。興建於1976年3月，於1980年10月興建完成。新山水庫為單一目標公共給水，供應基隆市、新北市汐止區（北山、橫科、宜興、福山、東勢、忠山及環河等七里除外）、瑞芳區瑞濱一帶等地區之用水。',
     descriptionEn:
       'The Xinshan Dam (traditional Chinese: 新山壩; simplified Chinese: 新山坝; pinyin: Xīnshān Bà) is a dam located in Anle District, Keelung, Taiwan. The dam supplies water to Keelung and New Taipei districts of Jinshan, Ruifang, Wanli and Xizhi Districts.',
-    locationImgPath: '../../../../assets/location-imgs/新山水庫.png',
+    locationImgPath: 'assets/location-imgs/新山水庫.png',
   },
   {
     id: 'reservoir20',
@@ -45,7 +45,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '寶山水庫，位於台灣新竹縣寶山鄉，頭前溪支流柴梳山溪上游，距離新竹市區東方約10公里。興建於1981年，1985年正式啟用，為一公共給水單目標之水庫，為新竹科學園區主要供水之來源。',
     descriptionEn:
       'The Baoshan Dam (traditional Chinese: 寶山壩; simplified Chinese: 宝山坝; pinyin: Bǎoshān Bà; Pe̍h-ōe-jī: Pó-san Chúi-khò͘) is a dam located in Baoshan Township, Hsinchu County, Taiwan. The dam supplies water to Hsinchu City and cooling water to factories in Hsinchu Science and Industrial Park.',
-    locationImgPath: '../../../../assets/location-imgs/寶山水庫.png',
+    locationImgPath: 'assets/location-imgs/寶山水庫.png',
   },
   {
     id: 'reservoir9',
@@ -53,7 +53,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '日月潭（邵語：Zintun 或 Zintun a Wazaqan[2]；閩南語：Ji̍t-goa̍t-thâm），是一個位在臺灣南投縣魚池鄉日月村的半天然淡水湖泊兼水力發電用水庫；該潭是臺灣本島面積第二大的湖泊（僅次於曾文水庫）及第一大半天然湖泊兼發電用水庫。該潭平均水面海拔約736公尺，常態面積約7.93平方公里（滿水位時約8.4平方公里），最高水深達27公尺。其蘊含自然生態豐富，但其中有非常多是外來種生物。',
     descriptionEn:
       'Sun Moon Lake (Chinese: 日月潭; pinyin: Rìyuè tán; Pe̍h-ōe-jī: Ji̍t-goa̍t-thâm; Thao: Zintun) is a lake in Yuchi Township, Nantou County, Taiwan. It is the largest body of water in Taiwan. The area around the lake is home to the Thao tribe, one of aboriginal tribes of Taiwan.Sun Moon Lake surrounds a tiny island called Lalu. The east side of the lake resembles a sun while the west side resembles a moon, hence the name.',
-    locationImgPath: '../../../../assets/location-imgs/日月潭水庫.png',
+    locationImgPath: 'assets/location-imgs/日月潭水庫.png',
   },
   {
     id: 'reservoir6',
@@ -61,14 +61,14 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '鯉魚潭水庫是臺灣的一座離槽水庫，位於大安溪支流景山溪下游，座落在苗栗縣三義鄉 、大湖鄉、卓蘭鎮內，集水區位於苗栗縣大湖鄉、卓蘭鎮與三義鄉，主要供應苗栗縣及臺中市的各種用水，同時具有觀光、灌溉、防洪、發電四大功能，壩高96公尺，集水面積53.45平方公里，總蓄水量1億2千6百萬立方公尺，於1992年11月完工，為苗栗縣境內最大的水庫。',
     descriptionEn:
       'The Liyutan Dam (traditional Chinese: 鯉魚潭壩; simplified Chinese: 鲤鱼潭坝; pinyin: Lǐyútán Bà) is a dam in Sanyi Township, Miaoli County, Taiwan.',
-    locationImgPath: '../../../../assets/location-imgs/鯉魚潭水庫.png',
+    locationImgPath: 'assets/location-imgs/鯉魚潭水庫.png',
   },
   {
     id: 'reservoir19',
     descriptionTw:
       '湖山水庫是臺灣一座離槽水庫，位於雲林縣斗六市及古坑鄉，位於斗六市東南方10公里，為全臺灣水庫中，有效容量第九大水庫，規劃除了水庫集水區(範圍小)外，並於清水溪建置桶頭攔河堰越域引水擴充水源，聯外道路工程已於2003年施作，大壩工程2008年開始進行，全部工程於2015年年底完工，於2016年4月2日啟用，主要用途為提供雲林縣、南彰化縣、嘉義縣市之用水(民生用水、農業灌溉及養殖漁業用水)，以減緩抽取地下水所造成地層下陷及地下水中「砷」汙染的問題，提高用水品質。完工後將與集集攔河堰共同調節水源，於豐水期自集集攔河堰引水，枯水期時則運用湖山水庫之蓄水。',
     descriptionEn: 'Hu-Shan reservoir.',
-    locationImgPath: '../../../../assets/location-imgs/湖山水庫.png',
+    locationImgPath: 'assets/location-imgs/湖山水庫.png',
   },
   {
     id: 'reservoir10',
@@ -76,7 +76,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '霧社水庫，又稱萬大水庫、碧湖，台灣一座位於南投縣仁愛鄉的水庫，係隸屬於台灣電力公司萬大發電廠所管理。因為水庫興建於霧社溪上而得名，加上水庫位處中央山脈群山之間，四周景致優雅秀麗，水色分外青碧，於是當年中華民國總統蔣中正取名為「碧湖」，而水庫主要功能為提供萬大發電廠發電之用，故又稱之為「萬大水庫」。',
     descriptionEn:
       'Wushe Dam (Chinese: 霧社壩; pinyin: Wùshè Bà) is a gravity dam forming Wushe Reservoir (霧社水庫; Wùshè Shuǐkù), also called Wanda Reservoir (萬大水庫; Wàndà Shuǐkù) and Bihu (碧湖; Bìhú), on the Wushe Creek (霧社溪; Wùshè Xī), a tributary of the Zhuoshui River, located in Ren-ai Township, Nantou County, Taiwan. The dam was completed in 1960 after seven years of construction, and serves mainly to generate hydroelectric power.',
-    locationImgPath: '../../../../assets/location-imgs/霧社水庫.png',
+    locationImgPath: 'assets/location-imgs/霧社水庫.png',
   },
   {
     id: 'reservoir3',
@@ -84,7 +84,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '永和山水庫是臺灣一座提供苗栗地區的水庫，位於苗栗縣三灣鄉永和村，原名東興水庫，應永和村（舊地名「永和山」）村民要求，完工後改名為永和山水庫。由台灣省水利局設計施工，建造於1980年（民國69年）7月，於1984年（民國73年）竣工啟用。水源主要來自主流中港溪田美攔河堰攔，經11公里導水路引入，是台灣首座沒有水門和水閘的水庫，為典型的離槽水庫。儲水供應地區為竹南鎮、頭份市、造橋鄉，為新竹苗栗地區重要之民生及工業用水的水庫。',
     descriptionEn:
       'The Yongheshan Reservoir (traditional Chinese: 永和山水庫; simplified Chinese: 永和山水库; pinyin: Yǒnghéshān Shuǐkù) is a reservoir in Sanwan Township, Miaoli County, Taiwan.',
-    locationImgPath: '../../../../assets/location-imgs/永和山水庫.png',
+    locationImgPath: 'assets/location-imgs/永和山水庫.png',
   },
   {
     id: 'reservoir5',
@@ -92,7 +92,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '明德水庫，臺灣一座位於苗栗縣頭屋鄉明德村的水庫，群山環抱，湖水清澈，還有遊艇可以遊覽。經水庫管理局多年來經營規劃，是一處知名的度假旅遊景點。環繞水庫的道路為縣道126的其中一段。',
     descriptionEn:
       'Mingde Dam (Chinese: 明德水壩; pinyin: Míngdé Shuǐbà) is a rockfill dam across the Laotianliao River in central Touwu Township, Miaoli County, Taiwan. The Mingde Reservoir (明德水庫 Míngdé Shuǐkù) behind the dam serves mainly for irrigation and municipal water supply for Miaoli City. The dam consists of a main embankment 35.5 metres (116 ft) high and 187 metres (614 ft) long, holding back a reservoir with a capacity of 17.1 million cubic metres (14,350 acre feet). The dam and reservoir control runoff from a catchment area of 61.08 square kilometres (23.58 sq mi). A spillway located to the north of the main dam consists of a six-bay gated overflow section, with a release capacity of 1,200 cubic metres per second (42,000 cu ft/s).',
-    locationImgPath: '../../../../assets/location-imgs/明德水庫.png',
+    locationImgPath: 'assets/location-imgs/明德水庫.png',
   },
   {
     id: 'reservoir8',
@@ -100,7 +100,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '石岡壩是台灣經濟部水利署中區水資源局所管理的攔河堰，於1977年完工營運，位在大甲溪下游，屬於臺中市境內，南岸屬石岡區，北岸為東勢區，流域面積1,061平方公里，約佔大甲溪全流域面積之85%，為大甲溪流域下游水資源運用樞紐；主要功能在於調節大甲溪流域尖峰發電尾水及河川水資源，並提供大台中地區公共給水及灌溉用水。',
     descriptionEn:
       'Shihgang Dam (traditional Chinese: 石岡壩; simplified Chinese: 石冈坝; pinyin: Shígāng Bà) is a concrete gravity barrage dam across the Dajia River in Shigang District and Dongshi District of Taichung, Taiwan, located near Fengyuan District. The dam was built from 1974 to 1977 for flood control and irrigation purposes, and stands 35.2 m (115 ft) high and 357 m (1,171 ft) long, holding a reservoir with an original capacity of 3,380,000 m3 (2,740 acre⋅ft).',
-    locationImgPath: '../../../../assets/location-imgs/石岡壩.png',
+    locationImgPath: 'assets/location-imgs/石岡壩.png',
   },
   {
     id: 'reservoir15',
@@ -108,7 +108,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '烏山頭水庫，舊稱烏山頭貯水池，是一座位於台灣台南市六甲區、官田區的水庫，具有水力發電功能，與烏山頭水力發電廠和烏山頭風景區相併在一起。地理位置在海拔468公尺的烏山嶺。由於水庫完成後，官田溪谷遭淹沒。自空中往俯看，其型珊瑚，故有珊瑚潭之美譽。',
     descriptionEn:
       'Wushantou Dam (Chinese: 烏山頭壩; pinyin: Wūshāntóu Bà) is an embankment dam in Guantian District, Tainan, Taiwan. The dam was designed by Yoichi Hatta and built between 1920 and 1930 during Japanese rule to provide irrigation water for the Chianan Plain as part of the Chianan Irrigation system. Because the natural flow of the Guantian River and other local streams was insufficient for irrigation of a planned 100,000 ha (250,000 acres), a tunnel was constructed to divert water from the Zengwen River to fill the reservoir. In 1974, the Zengwen Dam was completed on the Zengwen River shortly above the diversion tunnel, stabilizing and reducing the sediment load of water flowing into Wushantou Reservoir.',
-    locationImgPath: '../../../../assets/location-imgs/烏山頭水庫.png',
+    locationImgPath: 'assets/location-imgs/烏山頭水庫.png',
   },
   {
     id: 'reservoir21',
@@ -116,7 +116,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '蘭潭水庫，舊名紅毛埤，位於臺灣嘉義市東區鹿寮里，相傳為三百年前荷蘭人操練水軍之處，故稱「蘭潭」。水庫呈元寶狀，東西長約1公里，南北寬約2公里，佔地約2平方公里，屬離槽水庫，為嘉義地區重要的蓄水庫之一。「蘭潭泛月」名列嘉義八景之一。',
     descriptionEn:
       'Lantan Lake (traditional Chinese: 蘭潭水庫; simplified Chinese: 兰潭水库; pinyin: Lántán Shuǐkù) or Orchid Lake is a lake and reservoir in East District, Chiayi City, Taiwan.',
-    locationImgPath: '../../../../assets/location-imgs/蘭潭水庫.png',
+    locationImgPath: 'assets/location-imgs/蘭潭水庫.png',
   },
   {
     id: 'reservoir16',
@@ -124,14 +124,14 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '南化水庫是台灣台南市的一座水庫，水庫位於台南市南化區曾文溪之支流後堀溪上，集水面積104平方公里，總容量1億4946萬立方公尺，以阿里山山脈與曾文水庫相隔，兩水庫距離約10公里，共同擔負台南、高雄地區公共給水之重要水庫。',
     descriptionEn:
       'Nanhua Dam (Chinese: 南化水庫; pinyin: Nánhuà Shuǐkù; Pe̍h-ōe-jī: Lâm-hoà Chúi-khò͘), originally named Houjie Dam (後堀水庫), is a dam across the Houku River, a tributary of the Zengwen River in southern Taiwan. Located at Nanhua District, Tainan, the dam was built to provide municipal water to Tainan and Kaohsiung City and is operated by the Taiwan Water Corporation. Construction work started in December 1988, and impoundment of the reservoir began in July 1993. A conduit to supply water from the reservoir to Kaohsiung was completed in October 1993 and the entire project was operational in March 1994.',
-    locationImgPath: '../../../../assets/location-imgs/南化水庫.png',
+    locationImgPath: 'assets/location-imgs/南化水庫.png',
   },
   {
     id: 'reservoir14',
     descriptionTw:
       '曾文水庫是台灣嘉義縣的一座水庫，位於曾文溪上游大埔溪上。壩址位於大埔鄉西南部，集水區包括大埔鄉不含東南端、番路鄉東南部及東端、阿里山鄉西南部及中部，以及高雄市那瑪夏區西部凸出部分，水庫管理局則在臺南市楠西區境內，同時是臺灣庫容量最大的水庫。',
     descriptionEn: 'Zeng-Wen reservoir.',
-    locationImgPath: '../../../../assets/location-imgs/曾文水庫.png',
+    locationImgPath: 'assets/location-imgs/曾文水庫.png',
   },
   {
     id: 'reservoir13',
@@ -139,7 +139,7 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '白河水庫，是位於台灣台南市白河區的一座人工水庫，建造於1965年，係攔截急水溪上游之白水溪溪水而成。水庫潭面面積約170公頃，有效蓄水量1173萬立方公尺。是一座具備灌溉、防洪、給水等用途的多功能水庫，可提供台南市白河區、東山區及一部分後壁區的農田灌溉所需，並供應台灣紙業公司新營紙廠的工業用水，以及台灣自來水公司白河營運所的民生用水。2021年水庫清淤有成，庫容已恢復至1307萬立方公尺。',
     descriptionEn:
       'The Baihe Reservoir (traditional Chinese: 白河水庫; simplified Chinese: 白河水库; pinyin: Báihé Shuǐkù) is a reservoir in Baihe District, Tainan, Taiwan.',
-    locationImgPath: '../../../../assets/location-imgs/白河水庫.png',
+    locationImgPath: 'assets/location-imgs/白河水庫.png',
   },
   {
     id: 'reservoir12',
@@ -147,20 +147,20 @@ export const RESERVOIR_DESCRIPTION_LIST: ReservoirDetail[] = [
       '仁義潭水庫位於嘉義縣番路鄉八掌溪上游，屬離槽水庫，總容量達2,782萬立方公尺，集水面積 3.66平方公里，壩頂長度1535公尺，寬度9公尺，有效蓄水量2,506萬立方公尺。年供水量為五千二百萬公噸。 它是嘉義市及嘉義縣民雄地區公共給水及工業用水的主要來源，並具有調節蘭潭水庫貯水量的功能。',
     descriptionEn:
       'The Renyitan Dam (traditional Chinese: 仁義潭水壩; simplified Chinese: 仁义潭水坝; pinyin: Rényìtán Shuǐbà) is a dam in Fanlu Township, Chiayi County, Taiwan.',
-    locationImgPath: '../../../../assets/location-imgs/仁義潭水庫.png',
+    locationImgPath: 'assets/location-imgs/仁義潭水庫.png',
   },
   {
     id: 'reservoir17',
     descriptionTw:
       '阿公店水庫位於高雄市燕巢區小崗山東麓與岡山區、田寮區交界之阿公店溪上，主要功能為防洪與農田灌溉，為二次大戰後臺灣最早完成之水庫；公共給水部分，因水庫嚴重淤積及集水區污染，已停止供應；湖面廣達六百公頃，溢洪口採「天井式」漩渦出口，經暗管隧道排向阿公店溪，凸出水面的喇叭口極具特色。',
     descriptionEn: 'A-Gong-Dian Reservoir',
-    locationImgPath: '../../../../assets/location-imgs/阿公店水庫.png',
+    locationImgPath: 'assets/location-imgs/阿公店水庫.png',
   },
   {
     id: 'reservoir18',
     descriptionTw:
       '牡丹水庫，位於台灣屏東縣牡丹鄉四重溪上游，即恆春東北方約24公里處，係隸屬於經濟部水利署的一個水庫。牡丹水庫於1995年興建完成。增加之供水量足敷恆春地區迫切需要之水量。',
     descriptionEn: 'Mu-Dan Reservoir',
-    locationImgPath: '../../../../assets/location-imgs/牡丹水庫.png',
+    locationImgPath: 'assets/location-imgs/牡丹水庫.png',
   },
 ];
